84168e3ffc1fe71965a61eff7ade5005141dadac
Felipe Puentes <feliipepuentes@gmail.com>
2024-03-11 16:47:27 -0500

    Solving Exercise 3.5

---
+++ Chapter3/Exercise3-5/main.go
@@ -0,0 +1,47 @@
+// Exercise 3.5: Implement a full-color Mandelbrot set using the function image.NewRGBA amd the type
+// color.rGBA or color.YCbCr
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"math/cmplx"
+	"os"
+)
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+
+			// Image point (px, py) represents complex value z
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const constrat = 15
+
+	var v complex128
+
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.RGBA{R: 255 - constrat*n, G: 255 - constrat*n, B: 255, A: 255}
+		}
+	}
+	return color.RGBA{R: 255, G: 0, B: 0, A: 255}
+}
