eb76823c5a12dcce0b1c6aa0ba85bf6460153b45
Felipe Puentes <feliipepuentes@gmail.com>
2024-03-22 12:15:14 -0500

    solving exercise 6.4

---
+++ Chapter6/Exercise6-4/go.mod
@@ -0,0 +1,3 @@
+module Exercise6-4
+
+go 1.20
---
+++ Chapter6/Exercise6-4/main.go
@@ -0,0 +1,23 @@
+// Exercise 6.4 Add a method Elems that returns a slice containing the elements of the set,
+// suitable for iterating over with a range loop
+package main
+
+import "fmt"
+
+func (s *IntSet) Elems() []int {
+	var result []int
+	for i, word := range s.words {
+		for j := 0; j < 64; j++ {
+			if word&uint64(1<<j) != 0 {
+				result = append(result, 64*i+j)
+			}
+		}
+	}
+	return result
+}
+
+func main() {
+	var x IntSet
+	x.AddAll(1, 99, 5, 128)
+	fmt.Println(x.Elems())
+}
