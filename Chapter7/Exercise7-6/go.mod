c2c70ab433c1271f5268be5e933812036a5154ea
Felipe Puentes <feliipepuentes@gmail.com>
2024-03-25 21:40:06 -0500

    Solving Exercise 7.6

---
+++ Chapter7/Exercise7-6/go.mod
@@ -0,0 +1,3 @@
+module Exercise7-6
+
+go 1.20
---
+++ Chapter7/Exercise7-6/main.go
@@ -0,0 +1,42 @@
+// Exercise 7.6 Add support for Kelving temperatures to tempflag
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+type celsiusFlag struct{ Celsius }
+
+func (f *celsiusFlag) Set(s string) error {
+	var unit string
+	var value float64
+	fmt.Sscanf(s, "%f%s", &value, &unit)
+
+	switch unit {
+	case "C", "°C":
+		f.Celsius = Celsius(value)
+		return nil
+	case "F", "°F":
+		f.Celsius = FToC(Fahrenheit(value))
+		return nil
+	case "K", "°K":
+		f.Celsius = KToC(Kelvin(value))
+		return nil
+	}
+
+	return fmt.Errorf("invalid temperature %q", s)
+}
+
+func CelsiusFlag(name string, value Celsius, usage string) *Celsius {
+	f := celsiusFlag{value}
+	flag.CommandLine.Var(&f, name, usage)
+	return &f.Celsius
+}
+
+var temp = CelsiusFlag("temp", 20.0, "The temperature")
+
+func main() {
+	flag.Parse()
+	fmt.Println(*temp)
+}
